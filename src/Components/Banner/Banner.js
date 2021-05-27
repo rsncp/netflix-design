f95ff2e4aa573482e0acc488e2e014f26036e551
Ajmal Roshan <mohroshanajmal@gmail.com>
2021-05-27 21:46:03 +0530

    1st movie updated

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
+import {API_KEY,imageUrl} from '../../constants/constants'
+import axios from '../../axios'
 import './Banner.css'
 function Banner() {
+    const [movie, setMovie] = useState()
+    useEffect(() => {
+
+       axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+           console.log(response.data.results[0])
+        
+           setMovie(response.data.results[0])
+        })
+
+    }, [])
     return (
-        <div className='banner'>
+        <div
+        style={{backgroundImage: `url(${movie ? imageUrl+movie.backdrop_path :""})`}}
+        className='banner'>
             <div className='content'>
-                <h1 className='title'>Movie Name</h1>
+                <h1 className='title'>{movie ? movie.title :""}</h1>
                 <div className='banner_button'>
                     <button className='button'>Play</button>
                     <button className='button'>My List</button>
                 </div>
-                <h1 className='discription'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form</h1>
+                <h1 className='discription'>{movie ? movie.overview : "Following a zombie outbreak in Las Vegas, a group of mercenaries take the ultimate gamble: venturing into the quarantine zone to pull off the greatest heist ever attempted."}</h1>
             </div>
             <div className="fade_bottom"></div>
         </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+
+export const API_KEY ="e7d5eaf6a324d256948011e918ac4e45";
+
+export const imageUrl ='https://image.tmdb.org/t/p/original'
